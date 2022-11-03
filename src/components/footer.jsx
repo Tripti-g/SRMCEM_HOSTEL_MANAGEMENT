6790bcf12f203b5023a72f90a8837611b473fb82
narendraiiitl <lcs2019015@iiitl.ac.in>
2022-11-04 03:00:34 +0530

    github-pages

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <div style={{ height: '40px', backgroundColor: '#313331', width: '100%' }}></div>
+
+  )
+}
+
+export default Footer
